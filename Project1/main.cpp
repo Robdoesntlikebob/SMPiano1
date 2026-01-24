6e68e06eaf34488e5fb9def35112617797516686
Microsoft Swagmaster <imnotcustom789@gmail.com>
2026-01-24 21:11:30 +0000

    SO MUCH FUCKING POPPING
    added OOP just in case lol

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -6,14 +6,14 @@
 #include "sndemu/SPC_DSP.h"
 #include "sndemu/SPC_Filter.h"
 #include <SFML/Audio.hpp>
-
+using namespace sf;
 
 /*necessary variables*/
 typedef unsigned int uint;
 uint smppos = 0x200;
 SPC_DSP* dsp = new SPC_DSP;
 char* aram = (char*)malloc(0x10000);
-unsigned int dirposition = 0;
+uint dirposition = 0;
 
 
 unsigned char BRR_SAWTOOTH[] = {
@@ -39,12 +39,12 @@ unsigned char c700sqwave[] = {
 #define vadsr1(n) (n<<4)+5
 #define vadsr2(n) (n<<4)+6
 #define vgain(n) (n<<4)+7
+#define venvx(n) (n<<4)+8
+#define voutx(n) (n<<4)+9
 #define len(x) *(&x+1)-x
 #define lobit(x) x&0xff
 #define hibit(x) x>>8
-#define print(x) std::cout << x << std::endl;
-#define r(x) dsp->read(x)
-#define w(x,y) dsp->write(x,y)
+#define print(x) std::cout << x << std::endl
 
 
 enum {
@@ -52,20 +52,17 @@ enum {
     MVOLR = 0x1C,
     FLG = 0x6C,
     DIR = 0x5D,
-    V0VOLL = 0x00,
-    V0VOLR = 0x01,
-    V0PITCHL = 0x02,
-    V0PITCHH = 0x03,
-    V0SRCN = 0x04,
-    V0ADSR1 = 0x05,
-    V0ADSR2 = 0x06,
-    V0GAIN = 0x07,
     KON = 0x4C,
     KOF = 0x5C,
     ADSR = 128,
+    PMON = 0x2D,
+    NON = 0x3D,
+    ESA = 0x6D,
+    RESET = 128,
+    MUTE = 64,
+    ECEN = 32,
+    EON = 0x4D,
 };
-
-
 
 
 /*functions*/
@@ -81,22 +78,136 @@ void advance(unsigned int samples) {dsp->run(32*samples);}
 void advance() { dsp->run(32); }
 
 
-//void smp2aram(short* sample, uint length, uint lppoint) {
-//    memcpy(aram+smppos, sample, length);
-//    aram[0x1000 + dirposition++] = lobit(smppos);
-//    aram[0x1000 + dirposition++] = hibit(smppos);
-//    aram[0x1000 + dirposition++] = lobit(lppoint+smppos);
-//    aram[0x1000 + dirposition++] = hibit(lppoint+smppos);
-//    smppos += length;
-//}
+void smp2aram(unsigned char* sample, unsigned int length, unsigned int lppoint) {
+    memcpy(aram+smppos, sample, length);
+    aram[0x1000 + dirposition++] = lobit(smppos);
+    aram[0x1000 + dirposition++] = hibit(smppos);
+    aram[0x1000 + dirposition++] = lobit(lppoint+smppos);
+    aram[0x1000 + dirposition++] = hibit(lppoint+smppos);
+    smppos += length;
+}
 
 void tick(unsigned int ticks) {dsp->run(8534 * ticks);}
 void tick() { dsp->run(8534); }
+
+
+/*structures/classes*/
+
+class SPC700 : public SoundStream {
+public:
+    void load(const SoundBuffer& out) {
+        sfOut.assign(out.getSamples(), out.getSamples() + out.getSampleCount());
+        sfCSample = 0;
+        initialize(out.getChannelCount(), out.getSampleRate(), { SoundChannel::FrontLeft,SoundChannel::FrontRight });
+    }
+    struct sample {
+        uint dirstart, loloop, hiloop;
+    };
+protected:
+    typedef SPC_DSP::sample_t sample_t;
+    sample_t* out = (sample_t*)malloc(2 * sizeof(sample_t*));
+    uint smppos = 0x200;
+    uint vxkon;
+private:
+    /*DSP*/
+    SPC_DSP* d = new SPC_DSP;
+    char* ar = (char*)malloc(0x10000);
+    uint dirposition = 0;
+
+    SPC700() {
+    #undef w(x,y)
+    #undef r(x)
+    #undef lobit(x)
+    #undef hibit(x)
+    #define w(x,y) d->write(x,y)
+    #define r(x) d->read(x)
+    #define lobit(x) x&255
+    #define hibit(x) x>>8
+        d->init(ar);
+        d->set_output(out, 1);
+        for (int i = 0; i < SPC_DSP::register_count; i++) {
+            if (i == FLG) w(FLG, 0x60);
+            else if (i == ESA) w(ESA, 0x80);
+            else if (i == KOF) w(KOF, 0xff);
+            else w(i, 0);
+        }
+        w(DIR, 0xff); w(MVOLL, 127); w(MVOLR, 127); w(FLG, ECEN);
+    }
+
+    SPC700::sample newsample(unsigned char* sample, uint length, uint lppoint) {
+        SPC700::sample x = { 0,0,0 };
+        memcpy(ar + smppos, sample, length);
+        x.dirstart = 0xff00 + dirposition;
+        ar[0xff00 + dirposition++] = lobit(smppos);
+        ar[0xff00 + dirposition++] = hibit(smppos);
+        ar[0xff00 + dirposition++] = lobit(lppoint + smppos); x.loloop = hibit(smppos);
+        ar[0xff00 + dirposition++] = hibit(lppoint + smppos); x.hiloop = hibit(smppos);
+        smppos += length;
+        return x;
+    }
+    SPC700::sample changeloop(SPC700::sample sample, uint newloop) {
+        ar[sample.dirstart + 2] = lobit(newloop); sample.loloop = lobit(newloop);
+        ar[sample.dirstart + 3] = hibit(newloop); sample.loloop = hibit(newloop);
+        return sample;
+    }
+
+    void pitch(uint voice, uint pitch) {
+        w(vpl(voice), lobit(pitch));
+        w(vph(voice), hibit(pitch) & 0x3f);
+    }
+
+    void advance() { d->run(32); }
+    void advance(uint samples) { d->run(32*samples); }
+
+    void tick() { d->run(5334); }
+    void tick(uint ticks) { d->run(5334*ticks); }
+
+    void run() {
+        d->run(32);
+        if (out[-1] != 0) {
+            free(out);
+        }
+    }
+
+    int note(uint length_in_ticks, uint voice, uint pitch_in_hex, uint srcn, uint voll, uint volr) {
+        if (voice > 8 || voice < 0) print("can't write to this voice"); return -1;
+        if (voll > 127) print("VOLL too loud"); return -1; if (volr > 127) print("VOLR too loud"); return -1;
+        vxkon++;
+        w(vsrcn(voice), srcn);
+        w(vvoll(voice), std::trunc(voll/vxkon));
+        w(vvolr(voice), std::trunc(volr/vxkon));
+        return 0;
+    }
+
+    /*SFML*/
+    bool onGetData(Chunk &data) override {
+        const int count = 2;
+        data.samples = &sfOut[sfCSample];
+        if (sfCSample + count <= sfOut.size()) {
+            data.sampleCount = count;
+            data.samples += count;
+            return true;
+        }
+        else {
+            data.sampleCount = sfOut.size() - sfCSample;
+            sfCSample = sfOut.size();
+            return false;
+        }
+    }
+    void onSeek(Time offset) override {
+        sfCSample = static_cast<std::size_t>(offset.asSeconds() * getSampleRate() * getChannelCount());
+    }
+    std::vector<std::int16_t> sfOut;
+    std::size_t sfCSample{};
+};
+
 
 /*main*/
 
 
 int main() {
+    #define r(x) dsp->read(x)
+    #define w(x,y) dsp->write(x,y)
 
     /*initialising DSP and registers*/
     if (dsp == NULL) { std::cout << "no DSP lol" << std::endl; }
@@ -113,67 +224,62 @@ int main() {
 
     /*load instruments*/
 
-    memcpy(aram + smppos, BRR_SAWTOOTH, len(BRR_SAWTOOTH));
-    aram[0x1000 + dirposition++] = lobit(smppos);
-    aram[0x1000 + dirposition++] = hibit(smppos);
-    aram[0x1000 + dirposition++] = lobit(0 + smppos);
-    aram[0x1000 + dirposition++] = hibit(0 + smppos);
-    smppos += len(BRR_SAWTOOTH);
-
-    memcpy(aram + smppos, c700sqwave, len(c700sqwave));
-    aram[0x1000 + dirposition++] = lobit(smppos);
-    aram[0x1000 + dirposition++] = hibit(smppos);
-    aram[0x1000 + dirposition++] = lobit(9 + smppos);
-    aram[0x1000 + dirposition++] = hibit(9 + smppos);
-    smppos += len(c700sqwave);
+    smp2aram(BRR_SAWTOOTH, len(BRR_SAWTOOTH), 0);
+    smp2aram(c700sqwave, len(c700sqwave), 9);
 
 
     /*hopefully creating the DSP sound*/
     w(DIR, 0x10);
+    w(KOF, 0xff);
     w(MVOLL, 0x80);
     w(MVOLR, 0x80);
-    w(vvoll(0), 0x80); w(vvolr(1), 0x80); w(vvoll(1), 0x80);
-    w(vvolr(0), 0x80);
-    w(FLG, 0x20);
+    w(PMON, 0x00);
+    w(NON, 0x00);
+    w(EON, 0);
+    w(FLG, RESET);
+    w(FLG, ECEN);
+    w(vvoll(0), 0x3F); w(vvolr(0), 0x3F);
+    w(vvoll(1), 0x3F); w(vvolr(1), 0x3F);
     w(vsrcn(0), 1); w(vsrcn(1), 1);
-    pitch(0x107f,0); pitch(0xe00, 1);
     w(vadsr1(0), ADSR+0xA);
     w(vadsr2(0), 0xe0);
     w(vadsr1(1), ADSR + 0xA);
     w(vadsr2(1), 0xe0);
-    w(KON, 0b00000011);
+    pitch(0x10bf,0); pitch(0xe00, 1);
+    w(KOF, 0b11111100);
+    w(KON, 3);
     dsp->run(32*32000/4 - (256 * 32));
-    w(KOF, 1); advance(256); pitch(0x12bf, 0); w(KOF, 0); w(KON, 1);
+    w(KOF, 3); advance(256); pitch(0x12bf, 0); pitch(0x1000, 1); w(KOF, 0); w(KON, 3);
     dsp->run(32 * 32000/4 - (256*32));
-    w(KOF, 1); advance(256); pitch(0x14ff, 0); w(KOF, 0); w(KON, 1);
+    w(KOF, 3); advance(256); pitch(0x14ff, 0); pitch(0x10bf, 1); w(KOF, 0); w(KON, 3);
     dsp->run(32 * 32000 / 4 - (256*32));
-    w(KOF, 1); advance(256); pitch(0x167f, 0); w(KOF, 0); w(KON, 1);
+    w(KOF, 3); advance(256); pitch(0x167f, 0); pitch(0x12bf, 1); w(KOF, 0); w(KON, 3);
     dsp->run(32 * 32000 / 4 - (256*32));
-    w(KOF, 1); advance(256); pitch(0x18ff, 0); w(KOF, 0); w(KON, 1);
+    w(KOF, 3); advance(256); pitch(0x18ff, 0); pitch(0x14ff, 1); w(KOF, 0); w(KON, 3);
     dsp->run(32 * 32000 / 4 - (256*32));
-    w(KOF, 1); advance(256); pitch(0x1c40, 0); w(KOF, 0); w(KON, 1);
+    w(KOF, 3); advance(256); pitch(0x1c40, 0); pitch(0x167f, 1); w(KOF, 0); w(KON, 3);
     dsp->run(32 * 32000 / 4 - (256*32));
-    w(KOF, 1); advance(256); pitch(0x2000, 0); w(KOF, 0); w(KON, 1);
+    w(KOF, 3); advance(256); pitch(0x2000, 0); pitch(0x18ff, 1); w(KOF, 0); w(KON, 3);
     dsp->run(32 * 32000 / 4 - (256*32));
-    w(KOF, 1); advance(256); pitch(0x217f, 0); w(KOF, 0); w(KON, 1);
+    w(KOF, 3); advance(256); pitch(0x217f, 0); pitch(0x14ff, 1); w(KOF, 0); w(KON, 3);
     dsp->run(32 * 32000 / 4 - (256*32));
-    w(KOF, 1); advance(256);
+    w(KOF, 3); advance(256);
+    w(FLG, RESET);
 
     /*debug (remove when not needed)*/
-    //int generated_count = dsp->sample_count() / 2;
-    //printf("Generated %d samples\n", generated_count);
+    int generated_count = dsp->sample_count() / 2;
+    printf("Generated %d samples\n", generated_count);
 
-    //for (int i = 0; i < 10; i++) {
-    //    printf("%02d: %04hx %04hx\n", i, output[i * 2], output[1 + i * 2]);
-    //}
+    for (int i = 0; i < 10; i++) {
+        printf("%02d: %04hx %04hx\n", i, output[i * 2], output[1 + i * 2]);
+    }
 
 
     /*SFML side of playing sound*/
-    sf::SoundBuffer buf(output, 32 * 32000 * 2 + 16, 2, 32000, { sf::SoundChannel::FrontLeft,sf::SoundChannel::FrontRight });
+    sf::SoundBuffer buf(output, 32 * 32000 * 2, 2, 32000, { sf::SoundChannel::FrontLeft,sf::SoundChannel::FrontRight });
     sf::SoundBuffer bufwav("its gonna sound like shit trust me bro.wav");
     sf::Sound snd(buf);
-    snd.setLooping(0);
     snd.play();
-    sf::sleep(sf::milliseconds(2000));
+    sf::sleep(sf::milliseconds(2100));
 
 }
