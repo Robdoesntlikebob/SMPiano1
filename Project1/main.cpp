f848371fa4b8bda3376a1c12c335abe208b3ab4c
Microsoft Swagmaster <imnotcustom789@gmail.com>
2026-02-07 13:33:13 +0000

    update

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,104 +1,143 @@
+﻿#include <SFML/Audio.hpp>
+#include "sndEMU/SPC_DSP.h"
 #include "stdc++.h"
-#include <iostream>
-#include "sndEMU/dsp.h"
-#include <SFML/Audio.hpp>
+
+enum g_regs{
+	MVOLL = 0x0C,
+	MVOLR = 0x1C,
+	EVOLL = 0x2C,
+	EVOLR = 0x3C,
+	KON = 0x4C,
+	KOF = 0x5C,
+	FLG = 0x6C, //0x60
+	ENDX = 0x7C,
+	PMON = 0x2D,
+	NON = 0x3D,
+	EON = 0x4D,
+	DIR = 0x5D, //0x67 (SIX SEVEN)
+	ESA = 0x6D, //0x80
+	EDL = 0x7D,
+};
 
 unsigned char BRR_SAWTOOTH[] = {
-    0xB0, 0xFF, 0xEE, 0xDD, 0xCC, 0xBB, 0xAA, 0x99, 0x88,
-    0xB3, 0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11, 0x00,
+	0xB0, 0xFF, 0xEE, 0xDD, 0xCC, 0xBB, 0xAA, 0x99, 0x88,
+	0xB3, 0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11, 0x00,
 };
-#define len(x) *(&x+1)-x
-#define print(x) std::cout << x << std::endl
+
+unsigned char c700sqwave[] = {
+	0b10000100, 0x00, 0x00, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77,
+	0b11000000, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99,
+	0b11000000, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99,
+	0b11000000, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77,
+	0b11000011, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77
+};
 
 // Constants for the DSP registers.
-enum {
-    MVOLL = 0x0C,
-    MVOLR = 0x1C,
-    FLG = 0x6C,
-    DIR = 0x5D,
-    V0VOLL = 0x00,
-    V0VOLR = 0x01,
-    V0PITCHL = 0x02,
-    V0PITCHH = 0x03,
-    V0SRCN = 0x04,
-    V0ADSR1 = 0x05,
-    V0ADSR2 = 0x06,
-    KON = 0x4C,
-};
-
-class SPC700 : public sf::SoundStream {
-public:
-#define lobit(x) x&255
+#define vvoll(n) (n<<4)
+#define vvolr(n) (n<<4)+1
+#define vpl(n) (n<<4)+2
+#define vph(n) (n<<4)+3
+#define vsrcn(n) (n<<4)+4
+#define vadsr1(n) (n<<4)+5
+#define vadsr2(n) (n<<4)+6
+#define vgain(n) (n<<4)+7
+#define venvx(n) (n<<4)+8
+#define voutx(n) (n<<4)+9
+#define len(x) *(&x+1)-x
+#define lobit(x) x&0xff
 #define hibit(x) x>>8
-#define voll(x) (x<<4)
-#define volr(x) (x<<4)+1
-#define pl(x) (x<<4)+2
-#define ph(x) (x<<4)+3
-#define srcn(x) (x<<4)+4
-#define adsr1(x) (x<<4)+5
-#define adsr2(x) (x<<4)+6
-#define gain(x) (x<<4)+7
-#define envx(x) (x<<4)+8
-#define outx(x) (x<<4)+9
-#define r(x) spc_dsp_read(dsp,x)
-#define w(x,y) spc_dsp_write(dsp,x,y);
-#define run(x) spc_dsp_run(dsp,x);
+
+struct DSPStream : public sf::SoundStream
+{
+	SPC_DSP dsp{};
+	SPC_DSP::sample_t* buffer = (SPC_DSP::sample_t*)calloc(64000,sizeof(SPC_DSP::sample_t));
+	DSPStream()
+	{
+		dsp.init(aram.data());
+		initialize(2, 32000, { sf::SoundChannel::FrontLeft, sf::SoundChannel::FrontRight });
+		dsp.set_output(buffer, 64000);
+		dsp.reset();
+	}
+	bool onGetData(Chunk& data) override
+	{
+		dsp.set_output(buffer, 64000);
+		while (dsp.sample_count() < 64000)
+		{
+			dsp.run(512000);
+		}
+		data.sampleCount = 64000;
+		data.samples = buffer;
+		return true;
+	}
+
+	void onSeek(sf::Time timeOffset) override {};
+
+	void init() {
+		for (int i = 0; i < SPC_DSP::register_count; i++) {
+			if (i == KOF) dsp.write(KOF, 0xff);
+			else if (i == DIR) dsp.write(DIR, 0x67);
+			else if (i == FLG) dsp.write(FLG, 0x60);
+			else if (i == ESA) dsp.write(ESA, 0x80);
+			else dsp.write(i, 0);
+		}
+		dsp.write(MVOLL, 127), dsp.write(MVOLR, 127); dsp.write(FLG, 0x20);
+	}
+
+	void pitch(unsigned pitch, unsigned voice) {
+		dsp.write(vpl(voice), lobit(pitch));
+		dsp.write(vph(voice), hibit(pitch)&0x3f);
+	}
+
+	void newsample(unsigned char* sample, unsigned len, unsigned loop) {
+		memcpy(aram.data() + pos, sample, len);
+		aram.data()[0x6700 + dpos] = lobit(pos); dpos++;
+		aram.data()[0x6700 + dpos] = hibit(pos); dpos++;
+		aram.data()[0x6700 + dpos] = lobit(loop+pos); dpos++;
+		aram.data()[0x6700 + dpos] = hibit(loop+pos); dpos++;
+		pos += len;
+	}
+
+	void note(unsigned voice, bool hz, unsigned p, unsigned srcn, unsigned voll, unsigned volr, unsigned adsr1, unsigned adsr2) {
+		vxkon++;
+		dsp.write(vsrcn(0), srcn);
+		dsp.write(vvoll(0), voll/vxkon);
+		dsp.write(vvolr(0), volr/vxkon);
+		if (hz) pitch(pow(2, 12) * (p / 32000), voice);
+		else pitch(p, voice);
+		dsp.write(vadsr1(voice), adsr1);
+		dsp.write(vadsr2(voice), adsr2);
+		dsp.write(KON, 1 << voice);
+		dsp.write(KOF, 0 << voice);
+	}
+
+	void note(unsigned voice, bool hz, unsigned p, unsigned srcn, unsigned vol, unsigned adsr1, unsigned adsr2) {
+		vxkon++;
+		dsp.write(vsrcn(0), srcn);
+		dsp.write(vvoll(0), vol / vxkon);
+		dsp.write(vvolr(0), vol / vxkon);
+		if (hz) pitch(pow(2, 12) * (p / 32000), voice);
+		else pitch(p, voice);
+		dsp.write(vadsr1(voice), adsr1);
+		dsp.write(vadsr2(voice), adsr2);
+		dsp.write(KON, 1 << voice);
+		dsp.write(KOF, 0 << voice);
+	}
+
 private:
-    SPC_DSP* dsp = spc_dsp_new();
-    typedef spc_dsp_sample_t sample_t;
-    std::array<char, 0x10000 * sizeof(char)> aram{};
-    std::array<sample_t, 64000 * sizeof(sample_t)> out{};
-    bool onGetData(Chunk& data) override {
-        spc_dsp_set_output(dsp, out.data(), out.size());
-        while (spc_dsp_sample_count(dsp) > out.size()) {
-            run(512000);
-        }
-        data.sampleCount = out.size();
-        data.samples = out.data();
-        return true;
-    }
-    void onSeek(sf::Time x)override{}
-    unsigned dtpos = 0;
-    const unsigned dir = 0xff;
-    unsigned pos = 0x200;
-public:
-    SPC700() {
-        spc_dsp_init(dsp, aram.data());
-        spc_dsp_set_output(dsp, out.data(),out.size());
-        spc_dsp_reset(dsp);
-        initialize(2, 32000, { sf::SoundChannel::FrontLeft,sf::SoundChannel::FrontRight });
-    }
-    void newsample(unsigned char* sample, unsigned length, unsigned loop) {
-        memcpy(aram.data() + pos, sample, length);
-        aram.data()[dir + dtpos] = lobit(pos); dtpos++;
-        aram.data()[dir + dtpos] = hibit(pos); dtpos++;
-        aram.data()[dir + dtpos] = hibit(loop+pos); dtpos++;
-        aram.data()[dir + dtpos] = hibit(loop+pos); dtpos++;
-        pos += length;
-    }
-    void testplay() {
-        w(DIR, 0xff);
-        w(voll(0), 128);
-        w(volr(0), 128);
-        w(MVOLL, 128);
-        w(MVOLR, 128);
-        w(FLG, 0x20);
-        w(srcn(0), 0);
-        w(pl(0), 0xFF);
-        w(ph(0), 0x1F);
-        w(adsr1(0), 0x0F);
-        w(adsr2(0), 0xE0);
-        w(KON, 1);
-    }
-    void debug() {
-        print(spc_dsp_sample_count(dsp));
-    }
+	std::array<unsigned char, 0x10000> aram{};
+	#define aram aram.data();
+	unsigned dpos = 0;
+	unsigned vxkon = 0;
+	unsigned pos = 0x200;
 };
 
-int main() {
-    SPC700 emu;
-    emu.newsample(BRR_SAWTOOTH, len(BRR_SAWTOOTH),0);
-    emu.play();
-    emu.testplay();
-    while (emu.getStatus() == sf::SoundSource::Status::Playing) { emu.debug(); }
+int main()
+{
+	DSPStream emu;
+	emu.play();
+	emu.init();
+	emu.newsample(c700sqwave, len(c700sqwave), 9);
+	emu.note(0, 1, 32000, 0, 128, 0x0f, 0xe0);
+	while (emu.getStatus() == sf::SoundSource::Status::Playing) {}
+	return 0;
 }
