6e75d34346dbed946f670b9d84b9c272c7313d01
Microsoft Swagmaster <imnotcustom789@gmail.com>
2026-02-08 13:56:47 +0000

    upd

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -57,31 +57,13 @@ private:
 	unsigned pos = 0x200;
 public:
 	SPC_DSP dsp{};
-	std::array<SPC_DSP::sample_t, 2> buffer{};
+	std::array<SPC_DSP::sample_t, 32> buffer{};
 	SPC700()
 	{
 		dsp.init(aram.data());
 		initialize(2, 32000, { sf::SoundChannel::FrontLeft, sf::SoundChannel::FrontRight });
 		dsp.set_output(buffer.data(), buffer.size());
 		dsp.reset();
-	}
-	bool onGetData(Chunk& data) override
-	{
-		while (dsp.sample_count() < buffer.size())
-		{
-			dsp.run(32);
-			printf("0x%04X, 0x%04X\n", buffer[0], buffer[1]);
-			//print(dsp.sample_count());
-		}
-		dsp.set_output(buffer.data(), buffer.size());
-		data.sampleCount = buffer.size();
-		data.samples = buffer.data();
-		return true;
-	}
-
-	void onSeek(sf::Time timeOffset) override {};
-
-	void init() {
 		for (int i = 0; i < 0x7F; i++) {
 			if (i == KOF) dsp.write(KOF, 0xff);
 			else if (i == DIR) dsp.write(DIR, 0x67);
@@ -91,6 +73,21 @@ public:
 		}
 		dsp.write(MVOLL, 127), dsp.write(MVOLR, 127); dsp.write(FLG, 0x20);
 	}
+	bool onGetData(Chunk& data) override
+	{
+		while (dsp.sample_count() < buffer.size())
+		{
+			dsp.run(32);
+			printf("0x%04X, 0x%04X\n", buffer[0], buffer[1]);
+			//print(dsp.sample_count());
+		}
+		if (dsp.sample_count() >= buffer.size()) {
+		dsp.set_output(buffer.data(), buffer.size());
+		data.sampleCount = buffer.size();
+		data.samples = buffer.data();
+		return true;
+		}
+	} void onSeek(sf::Time timeOffset) override {};
 
 	void pitch(unsigned pitch, unsigned voice) {
 		dsp.write(vpl(voice), lobit(pitch));
@@ -136,10 +133,9 @@ public:
 int main()
 {
 	SPC700 emu;
-	emu.init();
 	emu.newsample(c700sqwave, len(c700sqwave), 9);
-	emu.note(0, 1, 32000, 0, 128, 128, 0x0f, 0xe0);
 	emu.play();
+	emu.note(0, 1, 32000, 0, 128, 0x0f, 0xe0);
 	while (emu.getStatus() == sf::SoundSource::Status::Playing) {}
 	return 0;
 }
